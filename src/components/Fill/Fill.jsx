8bd045569a92ae0b3fb0218e6af8cbfa20e6ff35
Laura Del valle <l.delvallebermejo@gmail.com>
2022-05-29 13:52:15 +0200

    grid

--- src/components/Fill/Fill.jsx
+++ src/components/Fill/Fill.jsx
@@ -4,149 +4,131 @@ import './Fill.css'
 
 const Fill = () => {
     return (
+        <>
+            <div className="gallery-container">
 
-        <div className="moments">
-
-            <div card>
-                <div className=''>
-                    <p>Momentos Danza Okara</p>
+                <div className='gallery-item'>
+                     <p className='grid-titulo'>Momentos Danza Okara</p> {/* //ok 1 */}
                 </div>
-            </div>
-            <div card>
-                <figure className='v-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737618/Danza%20Okara/foto29_b7lj18.jpg" alt="Festival" />
-                </figure>
 
-                <figure className='v-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737618/Danza%20Okara/foto20_j3dz7h.jpg" alt="Festival" />
-                </figure>
-            </div>
 
-            <div card>
-                <div className='content'>
-                    <p>Festivales Danza Okara</p>
+                <div className='gallery-item'>  {/* //ok 2*/}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737618/Danza%20Okara/foto29_b7lj18.jpg" alt="sori con abanicos" />
                 </div>
-            </div>
+                <div className='gallery-item'>  {/* //ok  3*/}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653664615/Danza%20Okara/IMG-20220516-WA0003_vppvl9.jpg" alt="Eva palmas" />
+                </div>
 
-            <div card>
-                <figure className='h-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651738937/Danza%20Okara/Grupo_Avanzado_ljuibt.jpg" alt="Festival" />
-                </figure>
-            </div>
+                <div className='gallery-item'>  {/* //ok 4*/}
+                    <p className='grid-texto'>Festivales!!!</p>
+                </div>
 
-            <div card>
+                <div className='gallery-item'> {/* //ok 5 */}
+                    <img className='gallery-top' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651738937/Danza%20Okara/Grupo_Avanzado_ljuibt.jpg" alt="Grupo avanzado" />
+                </div>
 
-                <figure className='big-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653319021/Danza%20Okara/WhatsApp_Image_2022-05-08_at_7.24.44_PM_s3rywb.jpg" alt="Festival" />
-                </figure>
-            </div>
-            
-            <div card>
-                <div className='content'>
-                    <h2>Disfruta bailando</h2>
+                <div className='gallery-item'>  {/* //ok 6 */}
+                    <img className='gallery-top' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651738875/Danza%20Okara/DanzaOkara-6_cudbke.jpg" alt="Bailarina naranja" />
+                </div>
+
+                <div className='gallery-item'>  {/* // 7 */}
+                    <img className='gallery-top' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651738847/Danza%20Okara/DanzaOkara-2_hjqyxy.jpg" alt="Sori y Conchi" />
+                </div>
+
+                <div className='gallery-item'>{/* // 8 */}
+                    <p className='grid-texto'> Siempre lo damos todo!</p>
+                </div>
+
+                <div className='gallery-item'>{/* // 9 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653313468/Danza%20Okara/WhatsApp_Image_2022-05-07_at_9.53.37_PM_pemtqg.jpg" alt="Diana brazo arriba" />
+                </div>
+
+                <div className='gallery-item'>{/* // 10 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653301817/Danza%20Okara/WhatsApp_Image_2022-03-22_at_11.34.51_AM_tl98ap.jpg" alt="Alas"/>
+                </div>
+
+                <div className='gallery-item'>{/* // 11 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653323007/Danza%20Okara/DanzaOkara-10_srknww.jpg" alt="Detras del telón" />
+                </div>
+
+                <div className='gallery-item'>{/* // 12 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737579/Danza%20Okara/foto2_vjisaa.jpg" alt="Conchi" />
+                </div>
+                <div className='gallery-item'> {/* // 13 */}
+                    <p className='grid-texto'>Halloween</p>
+                </div>
+
+                <div className='gallery-item'> {/* // 14 */}
+                    <img className='gallery-top' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653322033/Danza%20Okara/halloween_h6ru5y.jpg" alt="Halloween" />
+                </div>
+
+                <div className='gallery-item'> {/* // 15*/}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653322032/Danza%20Okara/halloween2_noumuv.jpg" alt="Halloween" />
+                </div>
+
+                <div className='gallery-item'> {/* // 16 */}
+                    <img className='gallery-top' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653311501/Danza%20Okara/DanzaOkara-30_qaicsu.jpg" alt="Carnaval" />
+                </div>
+
+                <div className='gallery-item'> {/* // 17 */}
+                    <p className='grid-texto'>Carnaval</p>
+                </div>
+
+                <div className='gallery-item'> {/* // 18 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653322035/Danza%20Okara/NAvidad_yonni0.jpg" alt="Navidad" />
+                </div>
+
+                <div className='gallery-item'> {/* // 19 */}
+                    <p className='grid-texto'>Navidad</p>
+                </div>
+                
+                <div className='gallery-item'>{/* // 20 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653319021/Danza%20Okara/WhatsApp_Image_2022-05-08_at_7.24.44_PM_s3rywb.jpg" alt="Festival" />
+                </div>
+
+                <div className='gallery-item'>{/* // 21 */}
+
+                    <div className='grid-llamada'>
+                    <h2 >Disfruta bailando</h2>
+                    <br/>
                     <p> Unete a nuestras locuras. Celebra las fiestas con el ritmo de la danza oriental. Navidad, carnaval, Halloween...
                         Y si se te ocurre alguna más que nos falte por disfrutar, avisanos!
                     </p>
                     <br />
-                    <p> Participamos en carreras solidarias, actuamos al aire libre, residencias, y en medio del parque.
+                    <p> Participamos en carreras solidarias, actuamos al aire libre, residencias, centros comerciales y hasta en medio del parque.
                         Para bailar siempre tenemos animos y ganas!
                     </p>
 
-                    <button>
+                    <button className="accion2">
                         Escribenos!
                     </button>
+                    </div>
 
+                </div>
+
+                <div className='gallery-item'> {/* // 19 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653664615/Danza%20Okara/IMG-20220516-WA0007_hoyl66.jpg" alt="alas plata" />
+                </div>
+
+                <div className='gallery-item'> {/* // 23 */}
+                    <img className='gallery-top' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737619/Danza%20Okara/foto23_gej491.jpg" alt="Velo azul" />
+                </div>
+
+                <div className='gallery-item'> {/* // 24 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653824202/Danza%20Okara/00a51146-8c66-4deb-8658-25589c039321_z42xeq.jpg" alt="Chicas del velo" />
+                </div>
+                <div className='gallery-item'> {/* // 23 */}
+                    <img className='gallery-top' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737618/Danza%20Okara/foto15_zi4osq.jpg" alt="Velo rojo" />
+                </div>
+
+                <div className='gallery-item'> {/* // 24 */}
+                    <img className='gallery-img' src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737579/Danza%20Okara/Foto8_dxtaht.jpg" alt="Fanny" />
                 </div>
 
 
             </div>
 
-
-            <div card>
-                <figure className='v-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737618/Danza%20Okara/foto19_qsmlqe.jpg" alt="Festival" />
-                </figure>
-                <figure className='v-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737618/Danza%20Okara/foto16_qepnd2.jpg" alt="Festival" />
-                </figure>
-            </div>
-
-            <div card>
-                <figure className='h-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653322033/Danza%20Okara/halloween_h6ru5y.jpg" alt="Halloween" />
-                </figure>
-                <div className='content'>
-                    <h3>Halloween</h3>
-                    <p>Siempre lo damos todo!</p>
-                </div>
-
-            </div>
-
-            <div card>
-                <figure className='v-strech'>
-
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653321743/Danza%20Okara/Foto9_ddwtaj.jpg" alt="Carrera" />
-                </figure>
-                <figure className='v-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653322032/Danza%20Okara/halloween2_noumuv.jpg" alt="Halloween" />
-                </figure>
-            </div>
-
-            <div card>
-                <div className='content'>
-                    <p>Ni durante la pandemia nos hemos podido quedar quietos.</p>
-                </div>
-            </div>
-
-            <div card>
-                <figure className='big-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653323007/Danza%20Okara/DanzaOkara-10_srknww.jpg" alt="Detras del telón" />
-                </figure>
-            </div>
-
-            <div card>
-                <div className='h-strech'>
-                    <p>Te estamos esperando</p>
-
-                </div>
-            </div>
-
-            <div card>
-                <figure className='v-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653322649/Danza%20Okara/pandemia_abammz.jpg" alt="Pandemia" />
-                </figure>
-            </div>
-
-            <div card>
-
-                <figure className='h-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653322034/Danza%20Okara/NAvidad2_o4cmbr.jpg" alt="Navidad" />
-                </figure>
-
-            </div>
-
-            <div card>
-                <figure className='v-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1653322035/Danza%20Okara/NAvidad_yonni0.jpg" alt="Navidad" />
-                </figure>
-            </div>
-
-            <div card>
-                <div className='content'>
-                    <p>Todo en el universo tiene ritmo. Todo baila.</p>
-                    <p> — Maya Angelou </p>
-                </div>
-            </div>
-
-            <div card>
-                <figure className='v-strech'>
-                    <img src="https://res.cloudinary.com/ddtolhmab/image/upload/v1651737619/Danza%20Okara/foto23_gej491.jpg" alt="Navidad" />
-                </figure>
-            </div>
-
-        </div>
-
-
+        </>
 
     )
 }
